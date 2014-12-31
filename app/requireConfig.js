4e0c2d529aea183f8cdd1ee5d699dfa3b272a3aa
Alexander Plesovskikh <me@hgen.ru>
2014-12-31 07:53:39 +0500

    fix path

--- app/requireConfig.js
+++ app/requireConfig.js
@@ -4,8 +4,7 @@ require.config({
         'knockout': '//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min',
         'pager': '//cdn.jsdelivr.net/pager.js/1.0.1/pager.min',
         'papaparse': '../vendor/papaparse',
-        'leaflet': '/bower_components/leaflet/dist/leaflet',
-        'leaflet.label': '/bower_components/Leaflet.label/dist/leaflet.label',
+        'leaflet': '//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet',
         'fuzzy': 'http://kiro.me/javascript/projects/fuse.min',
 
         'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min',
