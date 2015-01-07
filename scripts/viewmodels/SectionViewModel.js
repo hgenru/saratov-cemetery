c268f1ad715e5eb518498d70d15a32f25b3474d8
Alexander Plesovskikh <me@hgen.ru>
2015-01-08 02:06:39 +0500

    Update 2015-01-07T21:06:38.745Z

--- scripts/viewmodels/SectionViewModel.js
+++ scripts/viewmodels/SectionViewModel.js
@@ -26,6 +26,9 @@ define(
                 } else {
                     value = valueFloat;
                 }
+                if (!value) {
+                    value = newRow;
+                }
                 self.currentRow(value);
             };
 
